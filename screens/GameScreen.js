65367a396681036c964ab146cd5233f65d64cd6e
Mohammed Najib Yahya <mohammed.njeeb@gmail.com>
2022-11-16 14:09:33 +0300

    creating, using and displaying random numbers

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Title from "../components/Title";
+import Numbercontainer from "../components/game/NumberContainer";
+
+import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 
-const GameScreen = () => {
+const generateRandomBetween = (min, max, exclude) => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess!</Title>
-      {/* Guess */}
+      <Numbercontainer>
+        {currentGuess}
+      </Numbercontainer>
       <View>
         <Text>Higher or lower?</Text>
         {/* +
@@ -28,7 +46,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#ddb52f",
-    textAlign: 'center',
+    textAlign: "center",
     borderWidth: 2,
     borderColor: Colors.accent500,
     padding: 12,
