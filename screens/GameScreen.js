87b57bf7b566c12933a0ef068084cc80fcf38eb9
Mohammed Najib Yahya <mohammed.njeeb@gmail.com>
2022-11-17 16:23:09 +0300

    added icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import Numbercontainer from "../components/game/NumberContainer";
 import Card from "../components/ui/Card";
@@ -64,16 +65,18 @@ const GameScreen = ({ userNumber, onGameOver }) => {
       <Title>Opponent's Guess!</Title>
       <Numbercontainer>{currentGuess}</Numbercontainer>
       <Card>
-        <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+        <InstructionText style={styles.instructionText}>
+          Higher or lower?
+        </InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
+              <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
